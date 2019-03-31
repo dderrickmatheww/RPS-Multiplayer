8b3b3868ed5fda5e4936ef5435dc570bdefc1dd0
Derrick <dderrickmatheww@gmail.com>
2019-03-31 13:04:58 -0400

    update firebase

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCSHRXJZKjzTi266sw7RE_lf9N4y_u3zT4",
+    authDomain: "rps-multiplayer-5169c.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-5169c.firebaseio.com",
+    projectId: "rps-multiplayer-5169c",
+    storageBucket: "",
+    messagingSenderId: "489462411902"
+  };
+  
+  firebase.initializeApp(config);
